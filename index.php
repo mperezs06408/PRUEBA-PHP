b1bd8d4fa3a8981a66d90f53695501f64bd117b4
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-17 21:18:50 -0500

    Init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('Templantes/header.php')?>
+
+<div class="main">
+    <h1>Prueba de página</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate suscipit sequi id eum non perspiciatis dolorum magnam nesciunt aliquam reiciendis odit tempora, itaque ipsa sapiente saepe, autem quae possimus animi.
+    </p>
+</div>
+
+
+<?php include('Templantes/footer.php')?>
