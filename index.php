2e3df1588bcaf0ada8435f3de1c08ab253f8ab28
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-17 21:28:24 -0500

    four

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div class="main">
     <h1>Prueba de página</h1>
-    <p>
+    <p class="main">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate suscipit sequi id eum non perspiciatis dolorum magnam nesciunt aliquam reiciendis odit tempora, itaque ipsa sapiente saepe, autem quae possimus animi.
     </p>
 </div>
