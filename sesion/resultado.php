1434520d04f70d1bc87dd4d5815292fa6a605ff9
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-26 14:34:46 -0500

    1

--- sesion/resultado.php
+++ sesion/resultado.php
@@ -1,6 +1,6 @@
-<?php include('../Templantes/header.php')?>
+<?php include('Templantes/header.php')?>
 
 <?php include('../Conexion/conexion.php')?>
 
 
-<?php include('../Templantes/footer.php')?>
+<?php include('Templantes/footer.php')?>
