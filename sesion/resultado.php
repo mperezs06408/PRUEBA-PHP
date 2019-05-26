1ce02c584362869c0c8bcc38fdd205499c4a61ff
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-26 16:47:45 -0500

    PRUEBA 3

--- sesion/resultado.php
+++ sesion/resultado.php
@@ -4,22 +4,6 @@
 
 <div>
 
-    <?php
-        if (isset($_POST['nombre'] && isset($_POST['apellido'] && isset($_POST['email'] && isset($_POST['contraseña'])){
-            $sql= "INSERT INTO USUARIO (nombre,apellido,email,contrasena) VALUES (";
-            $sql=$sql."'"$_POST['nombre']."',";
-            $sql=$sql."'"$_POST['apellido']."',";
-            $sql=$sql."'"$_POST['email']."',";
-            $sql=$sql."'"$_POST['contrasena']."')";
-
-            if ($conexion ->query($sql) == TRUE) {
-                echo 'Usuario creado';
-            } else {
-                echo 'error';
-            }
-
-        }
-    ?>
 
 </div>
 
