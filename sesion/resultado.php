f6f692553989b60d85f4e4dcfcad9972863b9e9d
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-26 18:57:07 -0500

    5

--- sesion/resultado.php
+++ sesion/resultado.php
@@ -6,7 +6,7 @@
 <h1>hola</h1>
  <?php
         $sql= "INSERT INTO USUARIO (nombre,apellido,email,contrasena) VALUES ('".$_POST['nombre']."',".$_POST['apellido']."',".$_POST['email']."',".$_POST['contraseña']."')";
-
+        echo $sql;
     ?>
 
 </div>
