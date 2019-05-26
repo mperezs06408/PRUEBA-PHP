43727c22abf0d6b32fa1e073d0bb3900ecda1a89
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-26 18:43:31 -0500

    6

--- sesion/resultado.php
+++ sesion/resultado.php
@@ -4,16 +4,7 @@
 
 <div>
 <h1>hola</h1>
-    <?php
-        if (isset($_POST['nombre'] && isset($_POST['apellido'] && isset($_POST['email'] && isset($_POST['contraseña'])){
-            $sql= "INSERT INTO USUARIO (nombre,apellido,email,contrasena) VALUES (";
-            $sql=$sql."'"$_POST['nombre']."',";
-            $sql=$sql."'"$_POST['apellido']."',";
-            $sql=$sql."'"$_POST['email']."',";
-            $sql=$sql."'"$_POST['contrasena']."')";
 
-        }
-    ?>
 
 </div>
 
