c2b4778cdcc3604f285e65911b5a056d1dc8681f
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-26 14:38:50 -0500

    3

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -8,9 +8,9 @@ $db='PRUEBA';
 $conexion= new msqli($host,$user,$password,$db);
 
 if($conexion -> connect_errno){
-    echo '<h1>no hay conexion</h1>';
+    echo 'no hay conexion';
 } else {
-    echo '<h1>conectado</h1>';
+    echo 'conectado';
 }
 
 ?>
