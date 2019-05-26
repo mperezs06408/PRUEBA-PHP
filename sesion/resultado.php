df849c7b02ac934184e35cc77d5f5ef4de73e5ce
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-26 16:43:23 -0500

    Prueba inserción de datos

--- sesion/resultado.php
+++ sesion/resultado.php
@@ -2,5 +2,27 @@
 
 <?php include('../Conexion/conexion.php')?>
 
+<div>
+
+    <?php
+        if (isset($_POST['nombre'] && isset($_POST['apellido'] && isset($_POST['email'] && isset($_POST['contraseña'])){
+            $sql= "INSERT INTO USUARIO (nombre,apellido,email,contrasena) VALUES (";
+            $sql=$sql."'"$_POST['nombre']."',";
+            $sql=$sql."'"$_POST['apellido']."',";
+            $sql=$sql."'"$_POST['email']."',";
+            $sql=$sql."'"$_POST['contrasena']."')";
+
+            if ($conexion ->query($sql) == TRUE) {
+                echo 'Usuario creado';
+            } else {
+                echo 'error';
+            }
+
+            $conexion -> close();
+        }
+    ?>
+
+</div>
+
 
 <?php include('../Templantes/footer.php')?>
