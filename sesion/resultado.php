95d982337b4af3f4b97a2fb75372fc4fa9568bff
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-26 19:34:13 -0500

    prueba if

--- sesion/resultado.php
+++ sesion/resultado.php
@@ -6,13 +6,17 @@
 <h1>hola</h1>
  <?php
 
-
+    if (isset($_POST['name'])){
         $sql= "INSERT INTO USUARIO (nombre,apellido,email,contrasena) VALUES ('".$_POST['nombre']."','".$_POST['apellido']."','".$_POST['email']."','".$_POST['contrasena']."')";
         echo $sql;
         $conexion -> query($sql);
 
 
         $conexion ->close();
+    } else {
+        echo "no hay datos";
+    }
+
 
     ?>
 
