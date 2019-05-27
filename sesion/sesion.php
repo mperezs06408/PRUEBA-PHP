e1a8a03de083ee2db2e6fad423bdf2aff50cdd93
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-05-26 19:13:41 -0500

    prueba con name

--- sesion/sesion.php
+++ sesion/sesion.php
@@ -3,18 +3,18 @@
  <form action="resultado.php" method="POST">
   <div class="form-group">
     <label for="exampleInputEmail1">Nombre</label>
-    <input  class="form-control" id="nombre" aria-describedby="emailHelp" placeholder="Nombre">
+    <input  class="form-control" name="nombre" id="nombre" aria-describedby="emailHelp" placeholder="Nombre">
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Apellido</label>
-    <input  class="form-control" id="apellido" aria-describedby="emailHelp" placeholder="Apellido">
+    <input  class="form-control" name="apellido" id="apellido" aria-describedby="emailHelp" placeholder="Apellido">
   </div><div class="form-group">
     <label for="exampleInputEmail1">Email</label>
-    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
+    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Email">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Contraseña</label>
-    <input type="password" class="form-control" id="contrasena" placeholder="Contraseña">
+    <input type="password" class="form-control" name="contrasena" id="contrasena" placeholder="Contraseña">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
